7cb8487a922c8fa8c595cf73c3331dfcf3d7850f
mahdikh <mahdikhaloei5109@gmail.com>
2022-06-21 13:03:02 +0430

    add jwt for login

--- Life_B/urls.py
+++ Life_B/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace='home')),
     path('accounts/', include('authenticate.urls', namespace='authenticate')),
-    path('authenticate/', include('api.urls'))
+    path('authenticate/', include('api.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
